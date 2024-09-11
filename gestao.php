d2f4d725987c713e82d761f36e32317aa18764be
Leonardo da Silva Villa <leosivi@gmail.com>
2024-09-11 07:39:01 -0400

    2

--- gestao.php
+++ gestao.php
@@ -1,19 +1,40 @@
+<?php include "php/selectTarefas.php"; ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestão</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <?php include "components/navBar.php";?>
+    <?php include "components/navBar.php"; ?>
 
     <section>
-        <div><h1>Ínicio</h1></div>
-        <div><h1>Meio</h1></div>
-        <div><h1>Fim</h1></div>
+
+        <div id="coluna1">
+            <h4>Ínicio</h4>
+        </div>
+
+        <div id="coluna2">
+            <h4>Meio</h4>
+        </div>
+
+        <div id="coluna3">
+            <h4>Fim</h4>
+        </div>
+
     </section>
 
 </body>
+
 </html>
+<!-- Dados das tarefas em JSON -->
+<script id="tarefas" type="application/json">
+    <?= json_encode($registros) ?>
+</script>
+
+<!-- Referência ao arquivo JavaScript externo -->
+<script src="js/script.js"></script>
